d25777ec3d04bbbb1965b704deef632f9bf412da
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-06-21 16:31:28 -0300

    commit 5

--- drf_lapidacoes/escola/views.py
+++ drf_lapidacoes/escola/views.py
@@ -11,6 +11,8 @@ from escola.serializer import (
     ListaAlunosMatriculadosSerializer,
 )
 from rest_framework.response import Response
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 
 
 class AlunosViewSet(viewsets.ModelViewSet):
@@ -47,6 +49,10 @@ class MatriculaViewSet(viewsets.ModelViewSet):
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
     http_method_names = ["get", "post", "put", "path"]
+
+    @method_decorator(cache_page(20))
+    def dispatch(self, *args, **kwargs):
+        return super(MatriculaViewSet, self).dispatch(*args, **kwargs)
 
 
 class ListaMatriculasAluno(generics.ListAPIView):
